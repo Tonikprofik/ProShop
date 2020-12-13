b439fa38a5460aa378ac3b27d954b651eb6b13af
PonikTrofik <izanami369@gmail.com>
2020-12-13 14:46:02 +0100

    custom error handle middleware

--- back/server.js
+++ back/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 
@@ -15,6 +16,10 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/products', productRoutes)
+
+// custom error
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
