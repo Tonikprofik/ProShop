984296bdeea7e17cfa26b47e6303a897391390fd
tab <tab>
2020-12-16 22:11:47 +0100

    loader

--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { listProducts } from "../actions/productActions";
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import axios from "axios";
 // import products from "../products";
 
@@ -30,9 +32,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader>Loading...</Loader>
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
