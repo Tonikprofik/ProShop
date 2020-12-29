7e422c3bc5deb495fdd618da19553bd97c10a628
tab <12055230+Tonikprofik@users.noreply.github.com>
2020-12-29 15:24:06 +0100

    cart form delete action

--- front/src/screens/CartScreen.js
+++ front/src/screens/CartScreen.js
@@ -28,6 +28,10 @@ const CartScreen = ({ match, location, history }) => {
         }
     }, [dispatch, productId, qty])
 
+    const removeFromCartHandler = (id) => {
+        console.log(removeFromCartHandler)
+    }
+
     return (
         <Row>
             <Col md={8}>
@@ -46,8 +50,29 @@ const CartScreen = ({ match, location, history }) => {
                                 <Col md={3}>
                                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                                 </Col>
-                                
+
                                 <Col md={2}>${item.price}</Col>
+                                <Col md={2}>
+                                    <Form.Control
+                                        as='select'
+                                        value={item.qty}
+                                        onChange={(e) => dispatch(addToCart(item.product,
+                                            Number(e.target.value)))}
+                                    >
+                                        {[...Array(item.countInStock).keys()].map((x) => (
+                                            <option key={x + 1} value={x + 1}>
+                                                {x + 1}
+                                            </option>
+                                        ))}
+                                    </Form.Control>
+                                </Col>
+
+                                <Col md={2}>
+                                    <Button type='button' variant='light' onClick={() =>
+                                        removeFromCartHandler(item.product)}>
+                                        <i className='fas fa-trash'></i>
+                                    </Button>
+                                </Col>
 
                             </Row>
                         </ListGroup.Item>
