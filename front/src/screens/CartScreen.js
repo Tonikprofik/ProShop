1be58d33b7e6c07be9c53f5fb081008941fca045
tab <12055230+Tonikprofik@users.noreply.github.com>
2020-12-29 01:20:41 +0100

    cart empty, remove

--- front/src/screens/CartScreen.js
+++ front/src/screens/CartScreen.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import {addToCart} from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 
 
@@ -17,21 +17,50 @@ const CartScreen = ({ match, location, history }) => {
 
     const dispatch = useDispatch()
 
-    const cart = useSelector(state => state.cart )
-    //const {cartItems} = cart
-
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
     //console.log(cartItems);
 
     useEffect(() => {
-        if(productId) {
-            dispatch(addToCart(productId,qty))
+        if (productId) {
+            dispatch(addToCart(productId, qty))
         }
     }, [dispatch, productId, qty])
 
     return (
-        <div>
-            Cart
-        </div>
+        <Row>
+            <Col md={8}>
+                <h1>Shopping cart</h1>
+                {cartItems.length === 0 ? (
+                    <Message>
+                        Your cart is empty <Link to='/'>Go Back</Link>
+                    </Message>
+                ) : (<ListGroup variant='flush'>
+                    {cartItems.map(item => (
+                        <ListGroup.Item key={item.product}>
+                            <Row>
+                                <Col md={2}>
+                                    <Image src={item.image} alt={item.name} fluid rounded />
+                                </Col>
+                                <Col md={3}>
+                                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                </Col>
+                                
+                                <Col md={2}>${item.price}</Col>
+                            
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>)}
+            </Col>
+
+            <Col md={2}>
+            </Col>
+
+            <Col md={2}>
+            </Col>
+
+        </Row>
     )
 }
 
