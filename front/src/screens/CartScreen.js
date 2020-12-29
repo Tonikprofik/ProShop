6f99da1f9f14632d8c0e6ab8e8d077eca1d758f0
tab <12055230+Tonikprofik@users.noreply.github.com>
2020-12-30 00:13:57 +0100

    checkout btn, subtotal, reduce

--- front/src/screens/CartScreen.js
+++ front/src/screens/CartScreen.js
@@ -32,6 +32,11 @@ const CartScreen = ({ match, location, history }) => {
         console.log(removeFromCartHandler)
     }
 
+    const checkoutHandler = () => {
+        //console.log('checkout');
+        history.push('/login?redirect=shipping')
+    }
+
     return (
         <Row>
             <Col md={8}>
@@ -47,6 +52,7 @@ const CartScreen = ({ match, location, history }) => {
                                 <Col md={2}>
                                     <Image src={item.image} alt={item.name} fluid rounded />
                                 </Col>
+
                                 <Col md={3}>
                                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                                 </Col>
@@ -80,10 +86,24 @@ const CartScreen = ({ match, location, history }) => {
                 </ListGroup>)}
             </Col>
 
-            <Col md={2}>
-            </Col>
+            <Col md={4}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                            items</h2>
 
-            <Col md={2}>
+                        ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)
+                            .toFixed(2)}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Button type='button' className='btn-block'
+                            disabled={cartItems.length === 0}
+                            onClick={checkoutHandler}
+                        >
+                            Proceed To Checkout
+                        </Button>
+                    </ListGroup.Item>
+                </ListGroup>
             </Col>
 
         </Row>
