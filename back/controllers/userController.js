b35a3e5699e8ad48bcec092b259b9e5afe680588
PonikTrofik <izanami369@gmail.com>
2021-01-21 15:33:32 +0100

    get user login

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -32,7 +32,19 @@ const authUser = asyncHandler(async (req, res) => {
 // @ access Private
 
 const getUserProfile = asyncHandler(async (req,res) => {
-  res.send('yolo')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 })
 
 
