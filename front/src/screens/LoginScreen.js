1f75f5219083117d0216f757dd6dfe2eb7ee816d
PonikTrofik <izanami369@gmail.com>
2021-02-09 19:25:06 +0100

    login screen, form

---
+++ front/src/screens/LoginScreen.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+//emmet imrse
+//rafce
+import { Link } from "react-router-dom";
+import { Form, Button, Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { login } from "../actions/userActions";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password </Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+
+        <Row className="py-3">
+          <Col>
+            New Custormer?{' '}
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : "/register"}
+            >
+              Register
+            </Link>
+          </Col>
+        </Row>
+
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
